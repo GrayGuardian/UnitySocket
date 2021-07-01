056f55e33bdc4ed12c416f1e1b264b11a8494c23
xunyou <zjqtsw@163.com>
2021-07-01 18:07:45 +0800

    1

--- SocketClient/Assets/Scripts/Main.cs
+++ SocketClient/Assets/Scripts/Main.cs
@@ -12,29 +12,34 @@ public class Main : MonoBehaviour
 
         _client.OnDisconnect += () =>
         {
-            Text.text += "断开连接" + "/n";
+            Text.text += "断开连接" + "\n";
             UnityEngine.Debug.Log("断开连接");
         };
 
         _client.OnReceive += (data) =>
         {
+            Text.text += string.Format("接收到数据>>>{0}", data.Buff.Length) + "\n";
             UnityEngine.Debug.LogFormat("接收到数据>>>{0}", data.Buff.Length);
         };
         _client.OnError += (ex) =>
         {
+            Text.text += string.Format("出现异常>>>{0}", ex) + "\n";
             UnityEngine.Debug.LogFormat("出现异常>>>{0}", ex);
         };
 
         _client.OnReConnectSuccess += (num) =>
         {
+            Text.text += string.Format("第{0}次重连成功", num) + "\n";
             UnityEngine.Debug.LogFormat("第{0}次重连成功", num);
         };
         _client.OnReConnectError += (num) =>
         {
+            Text.text += string.Format("第{0}次重连失败", num) + "\n";
             UnityEngine.Debug.LogFormat("第{0}次重连失败", num);
         };
         _client.OnReconnecting += (num) =>
         {
+            Text.text += string.Format("正在进行第{0}次重连", num) + "\n";
             UnityEngine.Debug.LogFormat("正在进行第{0}次重连", num);
         };
 
@@ -42,10 +47,12 @@ public class Main : MonoBehaviour
 
         _client.Connect(() =>
         {
+            Text.text += "连接成功" + "\n";
             UnityEngine.Debug.Log("连接成功");
             // _client.DisConnect();
         }, () =>
         {
+            Text.text += "连接失败" + "\n";
             UnityEngine.Debug.Log("连接失败");
         });
 
